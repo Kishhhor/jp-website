3c2eb37711cc5f612fd70d53172bac8b8250f644
kishor <kishorkambleofficial@gmail.com>
2024-12-04 14:51:09 +0530

    All commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {  RouterLink, RouterOutlet } from '@angular/router';
+import {  NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet, Event } from '@angular/router';
 import { HomeComponent } from "./components/components/home/home.component";
 import { ServiceComponent } from "./components/components/service/service.component";
 import { AboutusComponent } from "./components/components/aboutus/aboutus.component";
@@ -7,13 +7,12 @@ import { CommonModule } from '@angular/common';
 import emailjs  from '@emailjs/browser'
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CareerComponent } from './components/components/career/career.component';
-
-
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, ServiceComponent,CareerComponent, AboutusComponent, RouterLink, CommonModule,ReactiveFormsModule],
+  imports: [RouterOutlet, HomeComponent, ServiceComponent,CareerComponent, AboutusComponent, RouterLink, CommonModule,ReactiveFormsModule, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -46,6 +45,10 @@ export class AppComponent  {
 
 // }
 
+
+
+
+
 form: FormGroup = this.fb.group({
   from_name: ['', Validators.required],
   to_name: ['Admin'],   
@@ -53,9 +56,25 @@ form: FormGroup = this.fb.group({
   from_phone:'',
   message: ['', Validators.required], 
 });
+  title: any;
+ 
 
-constructor(private fb: FormBuilder) {} 
+constructor(private fb: FormBuilder, 
+  private viewportScroller: ViewportScroller,
+  private router: Router,
+  
+  ) {} 
 
+
+
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        // Scroll to top on every route change
+        this.viewportScroller.scrollToPosition([0, 0]);
+      }
+    })
+  }
 
 async send() {
   emailjs.init('_A2IhqMJIUyQbtfaU');
@@ -70,4 +89,12 @@ async send() {
   alert('Message has been sent');
   this.form.reset();
 }
+
+scrollToFooter() {
+  const footer = document.getElementById('footer');
+  if (footer) {
+    footer.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { Routes } from '@angular/router';
 import { HomeComponent } from './components/components/home/home.component';
 import { ServiceComponent } from './components/components/service/service.component';
 import { AboutusComponent } from './components/components/aboutus/aboutus.component';
@@ -9,8 +8,9 @@ import { AIMLComponent } from './components/components/aiml/aiml.component';
 import { RoboticProcessAutomationComponent } from './components/components/robotic-process-automation/robotic-process-automation.component';
 import { WebDevelopmentComponent } from './components/components/web-development/web-development.component';
 import { DigitalMarketingComponent } from './components/components/digital-marketing/digital-marketing.component';
-import { NgModule } from '@angular/core';
 import { CareerComponent } from './components/components/career/career.component';
+import { AppComponent } from './app.component';
+
 
 //export const routes: Routes = [ 
 //     { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
@@ -38,24 +38,28 @@ import { CareerComponent } from './components/components/career/career.component
 
        
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'service', component: ServiceComponent },
-    { path: 'aboutus', component: AboutusComponent },
-    { path: 'career', component: CareerComponent },
-    { path: 'service/dataanalytics', component: DataAnalyticsComponent },
-    { path: 'service/businessintelligence', component: BusinessIntelligenceComponent },
-    { path: 'service/aiml', component: AIMLComponent },
-    { path: 'service/roboticprocessautomation', component: RoboticProcessAutomationComponent },
-    { path: 'service/webdevelopment', component: WebDevelopmentComponent },
-    { path: 'service/digitalmarketing', component: DigitalMarketingComponent },
     
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+        { path: 'home', component: HomeComponent },
+        { path: 'service', component: ServiceComponent },
+        { path: 'aboutus', component: AboutusComponent },
+        { path: 'career', component: CareerComponent },
+        { path: 'service/dataanalytics', component: DataAnalyticsComponent },
+        { path: 'service/businessintelligence', component: BusinessIntelligenceComponent },
+        { path: 'service/aiml', component: AIMLComponent },
+        { path: 'service/roboticprocessautomation', component: RoboticProcessAutomationComponent },
+        { path: 'service/webdevelopment', component: WebDevelopmentComponent },
+        { path: 'service/digitalmarketing', component: DigitalMarketingComponent },
+        
+        { path:'', redirectTo: '/home', pathMatch: 'full' }
+
+  
   ];
   
-  @NgModule({
-    imports: [RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',  // Enables smooth scrolling
-    })],
-    exports: [RouterModule]
-  })
+  // @NgModule({
+  //   imports: [RouterModule.forRoot(routes, {
+  //     scrollPositionRestoration: 'enabled',  // Enables smooth scrolling
+  //   })],
+  //   exports: [RouterModule]
+  // })
+
   export class AppRoutingModule { }
